1c83afff2e85b44c3c72282fa4fc74a14b97c22a
EricOliveiraSampaio <ericaosampaio25@gmail.com>
2025-09-15 23:52:52 -0300

    .

--- ProjetoLoja/Repository/ProdutoRepository.cs
+++ ProjetoLoja/Repository/ProdutoRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace ProjetoLoja.Repository
+﻿namespace ProjetoLoja.Repository;
+using ProjetoLoja.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+
+namespace ProjetoLoja.Repository
 {
     public class ProdutoRepository
     {
+        private readonly string _connectionString;
+
+        public ProdutoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Produto>> TodosProdutos()
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM Produtos";
+            return await connection.QueryAsync<Produto>(sql);
+        }
     }
 }
